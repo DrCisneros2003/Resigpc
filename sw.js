aaf83ef81a73c36b8ddbf415d410bdbee1313b9f
DrCisneros2003 <yisuspro260903@gmail.com>
2026-08-06 11:00:30 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "resigpc-v1";
+const CORE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return; // no cachear Supabase ni el Tutor
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
